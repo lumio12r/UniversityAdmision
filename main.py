aa668e71064a225563daee264e810e6c06df6649
lucas <lu.zatonski34@gmail.com>
2021-03-08 21:26:32 +0100

    Checking score

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ second_score = scores.append(int(input()))
 third_score = scores.append(int(input()))
 mean = statistics.mean(scores)
 print(mean)
-print("Congratulations, you are accepted!")
+if mean >= 60.00:
+    print("Congratulations, you are accepted!")
+else:
+    print("We regret to inform you that we will not be able to offer you admission.")
